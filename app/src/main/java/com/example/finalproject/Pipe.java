9d7987d184e8e78eafb4912eeba87cc6e5ee38df
levimoore22 <55932624+levimoore22@users.noreply.github.com>
2020-12-08 19:10:43 -0700

    Added help page (#8)
    * Added pipe layer class to randomly spawn pipes and added bounding box to bird and pipe for collisions
    
    * added how to play pic and back button
    
    * added changes
    
    Co-authored-by: lmoore22 <levimoore@email.arizona.edu>

--- app/src/main/java/com/example/finalproject/Pipe.java
+++ app/src/main/java/com/example/finalproject/Pipe.java
@@ -33,11 +33,11 @@ public class Pipe {
         return this.pipeImage;
     }
 
-    public void setY(int y) {
-        this.y = y;
-    }
+    public void setY(int y) { this.y = y; }
 
     public void setX(int x) {
         this.x = x;
     }
+
+
 }
